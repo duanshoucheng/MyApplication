63dd510efa216cde81ed21cf4a8b7aeb90075a8d
superDuan <dsc428@126.com>
2016-02-22 10:14:13 +0800

    Update Test.java

--- imgcomposition/src/main/java/com/example/administrator/imgcomposition/Test.java
+++ imgcomposition/src/main/java/com/example/administrator/imgcomposition/Test.java
@@ -4,5 +4,7 @@ package com.example.administrator.imgcomposition;
  * Created by Administrator on 2016-2-22.
  */
 public class Test {
-    public void print(){}
+    public String print(){
+        return "hello imgcomposition";
+    }
 }
